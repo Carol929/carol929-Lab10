319a2539826a6ee0e32f25a9ae9645bf2991c0e8
Carol Lu <Carol10051113@gmail.com>
2025-11-02 22:25:55 -0500

    setup for worldcounter

--- WordCounter.java
+++ WordCounter.java
@@ -1,3 +1,21 @@
+import java.io.IOException;
+import java.util.regex.Pattern;
+
 public class WordCounter {
-    
+    private static final Pattern WORD = Pattern.compile("[A-Za-z0-9']+");
+
+    public static void main(String[] args) {
+        
+    }
+    public static int processText(StringBuffer text, String stopword) throws InvalidStopwordException, TooSmallText{
+
+    }
+
+    public static StringBuffer processFile(String path) throws EmptyFileException {
+
+    }
+
+    public static StringBuffer processFile(String path) throws EmptyFileException {
+
+    }
 }
